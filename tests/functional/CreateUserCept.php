8805d0d40cbd8286703b0b23018be3df80e94a1c
24032620 <24032620@edgehill.ac.uk>
2020-04-14 17:08:25 +0100

    First functional test added - Create User

--- tests/functional/CreateUserCept.php
+++ tests/functional/CreateUserCept.php
@@ -7,7 +7,7 @@ $I->wantTo('create a new user');
 // When
 $I->amOnPage('admin/users');
 $I->see('Users', 'h1');
-$I->dontSee('Shaun Halliday');
+$I->dontSee('John Smith');
 // And
 $I->click('Add User');
 
@@ -16,5 +16,38 @@ $I->amOnPage('admin/users/create');
 // And
 $I->see('Add User', 'h1');
 $I->submitForm('.createuser', [
-    'name' =>
+    'name' => 'John Smith',
+    'email' => 'johnsmith@email.com',
+    'username' => 'JSmith123',
+    'password' => 'password123'
 ]);
+// Then
+$I->seeCurrentUrlEquals('admin/users');
+$I->see('Users', 'h1');
+$I->see('New user now added');
+$I->see('John Smith');
+
+
+// Check for duplicates
+
+ // When
+ $I->amOnPage('/admin/users');
+ $I->see('Users', 'h1');
+ $I->see('John Smith');
+ // And
+ $I->click('Add User');
+
+ // Then
+ $I->amOnPage('/admin/users/create');
+ // And
+ $I->see('Add User', 'h1');
+ $I->submitForm('.createuser', [
+   'name' => 'John Smith',
+   'email' => 'johnsmith@email.com',
+   'username' => 'JSmith123',
+   'password' => 'password123'
+ ]);
+ // Then
+ $I->seeCurrentUrlEquals('/admin/users');
+ $I->see('Users', 'h1');
+ $I->see('Error user already exists!');
