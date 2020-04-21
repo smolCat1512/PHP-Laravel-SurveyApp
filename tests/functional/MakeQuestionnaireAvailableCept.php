b997691fd13e5ea1a02a4033886206c7f4c09e15
24032620 <24032620@edgehill.ac.uk>
2020-04-21 14:15:11 +0100

    Draft completion of BDD tests

--- tests/functional/MakeQuestionnaireAvailableCept.php
+++ tests/functional/MakeQuestionnaireAvailableCept.php
@@ -1,3 +1,23 @@
-<?php 
+<?php
 $I = new FunctionalTester($scenario);
-$I->wantTo('perform actions and see result');
+
+$I->am('user');
+$I->wantTo('make a questionnaire live');
+
+// When
+$I->amOnPage('/admin/users/main/questionnaires');
+$I->see('Questionnaires', 'h1');
+$I->see('Test Questionnaire');
+$I->see('Development');
+$I->click('Test Questionnaire');
+
+// Then
+$I->amOnPage('/admin/users/main/questionnaires/Test Questionnaire');
+$I->seeButton('Make Questionnaire Live');
+$I->click('Make Questionnaire Live');
+
+// Then
+$I->seeCurrentUrlEquals('/admin/users/main/questionnaires');
+$I->see('Questionnaires', 'h1');
+$I->see('Test Questionnaire');
+$I->see('Live');
--- tests/functional/ViewResponsesCept.php
+++ tests/functional/ViewResponsesCept.php
@@ -1,3 +1,20 @@
-<?php 
+<?php
 $I = new FunctionalTester($scenario);
-$I->wantTo('perform actions and see result');
+
+$I->am('user');
+$I->wantTo('view responses');
+
+// When
+$I->amOnPage('/admin/users/main/responses');
+$I->see('Responses', 'h1');
+// And
+$I->click('View Responses');
+
+// create a questionnaire in the db that we can then update
+$I->haveRecord('answer', [
+    'id' => '0001',
+    'answer' => 'RandomAnswer',
+]);
+
+// Then
+$I->seeRecord('answers', ['answer' => 'RandomAnswer', 'id' => '0001']);
